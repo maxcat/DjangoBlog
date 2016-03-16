8ce5731a706bcf674ab4598ef307e60257448faa
EC2 Default User <ec2-user@ip-172-31-19-39.ap-southeast-1.compute.internal>
2016-03-16 08:19:15 +0000

    [DEV] add polls app.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,14 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites',
-    'blog',
 ]
 
 MIDDLEWARE_CLASSES = [
